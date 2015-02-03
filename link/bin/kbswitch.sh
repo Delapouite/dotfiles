1da34cdcd39909456967c2fdcda8ef31fb26fcfc
Delapouite <delapouite@gmail.com>
2015-02-03 10:56:45 +0100

    feat(kbswitch): add bepo handling

--- link/bin/kbswitch.sh
+++ link/bin/kbswitch.sh
@@ -16,3 +16,16 @@ xmodmap -e "keysym Caps_Lock = Control_L"
 xmodmap -e "add Control = Control_L"
 # default to -e "Control_L = Escape"
 ~/bin/xcape
+
+# switch xmonad bindings with
+if [[ `pgrep "xmonad"` ]]; then
+	echo "switching xmonad"
+	rm ~/.xmonad/xmonad.hs
+	if [[ "$1" == 'bepo' ]]; then
+		ln -s ~/.xmonad/xmonad.bepo.hs ~/.xmonad/xmonad.hs
+	else
+		ln -s ~/.xmonad/xmonad.azerty.hs ~/.xmonad/xmonad.hs
+	fi
+	xmonad --recompile
+	xmonad --restart
+fi
